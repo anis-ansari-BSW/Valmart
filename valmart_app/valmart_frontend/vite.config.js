38e54360a5fba95d33ee97fefad7bd264e38fc60
anis-ansari-BSW <anis.ansari@brand-scapes.com>
2025-05-20 12:51:10 +0530

    add vite config for live code

--- valmart_app/valmart_frontend/vite.config.js
+++ valmart_app/valmart_frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH === "production" ? "/valmart_frontend/" : "/",
   server: {
     proxy: {
       "/api": {
